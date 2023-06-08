46fd95e8fa7348ab3ae6d64c9190f60bb158ba2c
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-06-08 10:53:57 -0600

    updates to project02

--- controllers/lesson05.js
+++ controllers/lesson05.js
@@ -3,99 +3,143 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllVisits = async (req, res, next) => 
 {
-    const result = await mongodb
-        .getDb()
-        .db('project02')
-        .collection('doc_visits')
-        .find();
-    result.toArray().then((lists) => 
+    try
     {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists);
-    });
+        const result = await mongodb
+            .getDb()
+            .db('project02')
+            .collection('doc_visits')
+            .find();
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists);
+        });
+    }
+    catch(error)
+    {
+        res.status(400).json({message:"Get all visits error, unexpected"})
+    }
 };
 
-/*
-const getContactByDate = async (req, res, next) => 
+const getVisitById = async (req, res, next) => 
 {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db('DBname')
-        .collection('Contacts')
-        .find({_id: userId});
-    result.toArray().then((lists) => 
+    if (!ObjectId.isValid(req.params.id)) 
     {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
-    });
+        ('Invlaid ID');
+        
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .db('project02')
+            .collection('doc_visits')
+            .find({_id: userId});
+        result.toArray().then((lists) => 
+        {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+    }
 };
-*/
 
 const postNewVisit = async (req, res, next) =>
 {
-    const visit = 
+    try
     {
-        date: req.body.date,
-        doc_fname: req.body.doc_fname,
-        doc_lname: req.body.doc_lname,
-        email: req.body.email,
-        prescription: req.body.prescription,
-        refillable: req.body.refillable
+        if(!req.body.fname)
+        {
+            throw new Error('First name is required')
+        }
+        const visit = 
+        {
+            date: req.body.date,
+            doc_fname: req.body.doc_fname,
+            doc_lname: req.body.doc_lname,
+            email: req.body.email,
+            prescription: req.body.prescription,
+            refillable: req.body.refillable
+        }
+        const result = await mongodb
+            .getDb()
+            .db('project02')
+            .collection('doc_visits')
+            .insertOne(visit);
+        if(result.acknowledged)
+        {
+            res.status(201).json(result)
+        }
+        else
+        {
+            res.status(500).json(result.error)
+        }
     }
-    const result = await mongodb
-        .getDb()
-        .db('project02')
-        .collection('doc_visits')
-        .insertOne(visit);
-    if(result.acknowledged)
+    catch(error)
     {
-        res.status(201).json(result)
-    }
-    else
-    {
-        res.status(500).json(result.error)
+        res.status(400).json({message:error.message});
     }
 }
 
 const updateVisit = async (req, res) =>
 {
-    const userId = new ObjectId(req.params.id);
-  // be aware of updateOne if you only want to update specific fields
-  const visit = {
-    date: req.body.date,
-    doc_fname: req.body.doc_fname,
-    doc_lname: req.body.doc_lname,
-    email: req.body.email,
-    prescription: req.body.prescription,
-    refillable: req.body.refillable
-  };
-  const response = await mongodb
-    .getDb()
-    .db('project02')
-    .collection('doc_visits')
-    .replaceOne({ _id: userId }, visit);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
-  }
+    try
+    {
+        if (!ObjectId.isValid(req.params.id)) 
+        {
+            throw new Error('Invalid ID');
+        }
+
+            const userId = new ObjectId(req.params.id);
+        // be aware of updateOne if you only want to update specific fields
+        const visit = {
+            date: req.body.date,
+            doc_fname: req.body.doc_fname,
+            doc_lname: req.body.doc_lname,
+            email: req.body.email,
+            prescription: req.body.prescription,
+            refillable: req.body.refillable
+        };
+        const response = await mongodb
+            .getDb()
+            .db('project02')
+            .collection('doc_visits')
+            .replaceOne({ _id: userId }, visit);
+        console.log(response);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+        }
+    }
+    catch(error)
+    {
+        res.status(400).json({message:"Get all visits error, unexpected"});
+    }
 };
 
 const deleteVisitById = async (req, res, next) =>
 {
-    const userId = new ObjectId(req.params.id);
-    const response = await mongodb
-    .getDb()
-    .db('project02')
-    .collection('doc_visits')
-    .deleteOne({ _id: userId }, true);
-    console.log(response);
-    if (response.deletedCount > 0) {
-        res.status(200).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    try
+    {
+        if (!ObjectId.isValid(req.params.id)) 
+        {
+            throw new Error('Invalid ID');
+        }
+        const userId = new ObjectId(req.params.id);
+        const response = await mongodb
+        .getDb()
+        .db('project02')
+        .collection('doc_visits')
+        .deleteOne({ _id: userId }, true);
+        console.log(response);
+        if (response.deletedCount > 0) {
+            res.status(200).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+        }
+    }
+    catch(error)
+    {
+        res.status(400).json({message:error.message});
     }
 };
 
@@ -104,5 +148,6 @@ module.exports =
     getAllVisits,
     postNewVisit,
     updateVisit,
-    deleteVisitById
+    deleteVisitById,
+    getVisitById
 };
